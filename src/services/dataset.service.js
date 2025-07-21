335d2e8113dd5bbd68f3d479fd332856fd146511
gabrielNZT <gabrielnunescesarino@gmail.com>
2025-07-21 00:57:23 -0300

    CHG: Improments single responsability

--- src/services/dataset.service.js
+++ src/services/dataset.service.js
@@ -1,5 +1,6 @@
 const prisma = require('../lib/prisma');
 const fs = require('fs');
+const { parseFileContent } = require('../utils/fileProcessor');
 
 const createDataset = async (file, userId) => {
   const dataset = await prisma.dataset.create({
@@ -9,31 +10,14 @@ const createDataset = async (file, userId) => {
     },
   });
 
-  const records = [];
-  if (file.mimetype === 'application/pdf') {
-    records.push({ jsonData: { content: fs.readFileSync(file.path, 'base64') } });
-  } else if (file.mimetype === 'text/csv') {
-    const csvData = fs.readFileSync(file.path, 'utf-8');
-    const rows = csvData.split('\n').map(row => row.split(','));
-    const headers = rows[0];
-    for (let i = 1; i < rows.length; i++) {
-      const values = rows[i];
-      const record = {};
-      for (let j = 0; j < headers.length; j++) {
-        record[headers[j]] = values[j];
-      }
-      records.push({ jsonData: record });
-    }
-  }
+  const jsonData = await parseFileContent(file);
 
-  for (const record of records) {
-    await prisma.record.create({
-      data: {
-        datasetId: dataset.id,
-        jsonData: record.jsonData,
-      },
-    });
-  }
+  await prisma.record.create({
+    data: {
+      datasetId: dataset.id,
+      jsonData,
+    },
+  });
 
   fs.unlinkSync(file.path);
 
---
+++ src/utils/fileProcessor.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const parseFileContent = async (file) => {
+    const PDF_MIME_TYPE = 'application/pdf';
+    const CSV_MIME_TYPE = 'text/csv';
+
+    const { size } = fs.statSync(file.path);
+    const type = file.mimetype;
+
+    if (type === PDF_MIME_TYPE) {
+        const content = fs.readFileSync(file.path, 'base64')
+        return { content, size };
+    }
+
+    if (type === CSV_MIME_TYPE) {
+        const fileContent = fs.readFileSync(file.path, 'utf-8');
+        const rows = fileContent.split('\n').map(row => row.split(','));
+        const headers = rows[0];
+
+        const records = rows.slice(1).map(values => {
+            const record = {};
+            headers.forEach((header, index) => {
+                record[header] = values[index];
+            });
+            return record;
+        });
+
+        return { content: records, size };
+    }
+
+    throw new Error('Unsupported file type');
+};
+
+module.exports = {
+    parseFileContent,
+};
